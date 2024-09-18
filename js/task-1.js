a66d919cb40f909ebd927d9cf50cb23630210d79
Serhiy Malysh <166062683+finconsultsm@users.noreply.github.com>
2024-09-18 07:13:26 +0200

    4

--- js/task-1.js
+++ js/task-1.js
@@ -1,21 +1,20 @@
-function slugify(title) {
- const words = title
- .toLowerCase()
- .trim()
- .split(' ');
-
- const nonEmptyWords = [];
-
- for(const word of words) {
- if (word.length > 0) {
-    nonEmptyWords.push(word)
- }
+function isEnoughCapacity(products, containerSize) {
+   const totalProducts = Object.values(products).reduce((total, quantity) => total + quantity, 0);
+   return totalProducts <= containerSize;
 }
 
-return nonEmptyWords.join('-');
-}
+console.log(
+   isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8)
+ );
 
-console.log(slugify("Arrays for beginners")); // "arrays-for-beginners"
-console.log(slugify("English for developer")); // "english-for-developer"
-console.log(slugify("Ten secrets of JavaScript")); // "ten-secrets-of-javascript"
-console.log(slugify("How to become a JUNIOR developer in TWO WEEKS")); // "how-to-become-a-junior-developer-in-two-weeks"
+ console.log(
+   isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12)
+ ); 
+ 
+ console.log(
+   isEnoughCapacity({ apples: 1, lime: 5, tomatoes: 3 }, 14)
+ ); 
+ 
+ console.log(
+   isEnoughCapacity({ apples: 18, potatoes: 5, oranges: 2 }, 7)
+ ); 
